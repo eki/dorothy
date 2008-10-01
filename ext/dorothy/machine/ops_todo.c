7b9cfd296aca04969926c5aa3ed242a5003d123a
Eric K Idema <eki@vying.org>
2008-10-01 14:50:56 -0400

    Update ops_todo (remove read / random instructions, change some instructions to halt execution with a not implemented error).

--- ext/dorothy/machine/ops_todo.c
+++ ext/dorothy/machine/ops_todo.c
@@ -2,155 +2,128 @@
 #include "machine.h"
 
 void z_buffer_mode( zmachine *zm ) {
-
 }
 
 void z_check_unicode( zmachine *zm ) {
-
+  runtime_error( "z_check_unicode is not yet implemented!" );
 }
 
 void z_draw_picture( zmachine *zm ) {
-
-}
-
-void z_encode_text( zmachine *zm ) {
-
+  runtime_error( "z_draw_picture is not yet implemented!" );
 }
 
 void z_erase_line( zmachine *zm ) {
-
+  runtime_error( "z_erase_line is not yet implemented!" );
 }
 
 void z_erase_picture( zmachine *zm ) {
-
+  runtime_error( "z_erase_picture is not yet implemented!" );
 }
 
 void z_erase_window( zmachine *zm ) {
-
+  runtime_error( "z_erase_window is not yet implemented!" );
 }
 
 void z_get_cursor( zmachine *zm ) {
-
+  runtime_error( "z_get_cursor is not yet implemented!" );
 }
 
 void z_get_wind_prop( zmachine *zm ) {
-
+  runtime_error( "z_get_wind_prop is not yet implemented!" );
 }
 
 void z_input_stream( zmachine *zm ) {
-
+  runtime_error( "z_input_stream is not yet implemented!" );
 }
 
 void z_make_menu( zmachine *zm ) {
-
+  runtime_error( "z_make_menu is not yet implemented!" );
 }
 
 void z_mouse_window( zmachine *zm ) {
-
+  runtime_error( "z_mouse_window is not yet implemented!" );
 }
 
 void z_move_window( zmachine *zm ) {
-
+  runtime_error( "z_move_window is not yet implemented!" );
 }
 
 void z_output_stream( zmachine *zm ) {
-
 }
 
 void z_picture_data( zmachine *zm ) {
-
+  runtime_error( "z_picture_data is not yet implemented!" );
 }
 
 void z_picture_table( zmachine *zm ) {
-
+  runtime_error( "z_picture_table is not yet implemented!" );
 }
 
 void z_put_wind_prop( zmachine *zm ) {
-
-}
-
-void z_read( zmachine *zm ) {
-  runtime_error( "z_read is not yet implemented!" );
-}
-
-void z_read_char( zmachine *zm ) {
-  runtime_error( "z_read_char is not yet implemented!" );
+  runtime_error( "z_put_wind_prop is not yet implemented!" );
 }
 
 void z_read_mouse( zmachine *zm ) {
-
+  runtime_error( "z_read_mouse is not yet implemented!" );
 }
 
 void z_restart( zmachine *zm ) {
-
+  runtime_error( "z_restart is not yet implemented!" );
 }
 
 void z_restore( zmachine *zm ) {
-
+  runtime_error( "z_restore is not yet implemented!" );
 }
 
 void z_restore_undo( zmachine *zm ) {
-
+  runtime_error( "z_restore_undo is not yet implemented!" );
 }
 
 void z_save( zmachine *zm ) {
-
+  runtime_error( "z_save is not yet implemented!" );
 }
 
 void z_save_undo( zmachine *zm ) {
-
 }
 
 void z_scroll_window( zmachine *zm ) {
-
+  runtime_error( "z_scroll_window is not yet implemented!" );
 }
 
 void z_set_font( zmachine *zm ) {
-
 }
 
 void z_set_colour( zmachine *zm ) {
-
 }
 
 void z_set_cursor( zmachine *zm ) {
-
 }
 
 void z_set_margins( zmachine *zm ) {
-
+  runtime_error( "z_set_margins is not yet implemented!" );
 }
 
 void z_set_window( zmachine *zm ) {
-
 }
 
 void z_set_text_style( zmachine *zm ) {
-
 }
 
 void z_show_status( zmachine *zm ) {
-
 }
 
 void z_sound_effect( zmachine *zm ) {
-
 }
 
 void z_split_window( zmachine *zm ) {
-
-}
-
-void z_tokenise( zmachine *zm ) {
-
 }
 
 void z_window_size( zmachine *zm ) {
-
+  runtime_error( "z_window_size is not yet implemented!" );
 }
 
 void z_window_style( zmachine *zm ) {
-
+  runtime_error( "z_window_style is not yet implemented!" );
 }
 
 

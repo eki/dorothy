9ca40896346a8b93362705e1ec855910cb72921d
Eric K Idema <eki@vying.org>
2008-10-01 14:52:27 -0400

    Add InputStream, set Machine#keyboard to an inputstream.  Add Machine#run (call step repeatedly until it returns false).

--- lib/dorothy/machine.rb
+++ lib/dorothy/machine.rb
@@ -1,9 +1,51 @@
 
 class Machine
 
-  attr_reader :header, :dictionary, :output, :trace
+  attr_reader :header, :dictionary, :output, :trace, :keyboard
 
+  def run
+    loop do
+      break unless step
+    end
+  end
 
+  class InputStream
+    attr_reader :id
+
+    def initialize( id )
+      @id, @str = id, ""
+    end
+
+    def <<( s )
+      @str << s
+    end
+
+    def line_available?
+      @str.index "\n"
+    end
+
+    def char_available?
+      @str.length > 1
+    end
+
+    def read_line( max=nil )
+      i = line_available?
+
+      return ""  if i.nil?
+
+      if max.nil? || i <= max
+        s = @str.slice!( 0, i )
+        t = @str.slice!( 0, 1 )
+        s
+      else
+        @str.slice!( 0, max )
+      end
+    end
+
+    def read_char
+      @str.slice!( 0, 1 )[0]
+    end
+  end
 
 end
 

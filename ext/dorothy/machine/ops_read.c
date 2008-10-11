f8f3856be14f6605429bb8692fcb9b1550df0f12
Eric K Idema <eki@vying.org>
2008-10-11 00:05:39 -0400

    Added code to echo the prompt and input to output (during a z_read).  This should probably be optional.

--- ext/dorothy/machine/ops_read.c
+++ ext/dorothy/machine/ops_read.c
@@ -96,6 +96,14 @@ void z_read( zmachine *zm ) {
     write_parse_table( zm, line, zm->zargs[1], h_dictionary(zm), 0 );
   }
 
+  /* echo the input back to the output */
+
+  print_cstr( zm, "> " );
+  print_rstr( zm, line );
+  print_cstr( zm, "\n" );
+
+  /* store the terminating character (if required) */
+
   if( h_version(zm) > 4 ) {
     p_store( zm, translate_to_zscii( zm, 10 ) );
   }

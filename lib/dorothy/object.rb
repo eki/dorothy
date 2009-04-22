072fb70ded655b3d1589f0caa6f150f7454534ae
Eric K Idema <eki@vying.org>
2009-04-22 12:58:07 -0400

    Added Z::Object#parent?, #siblings?, #children?, #siblings, #children, #root, and #inspect.

--- lib/dorothy/object.rb
+++ lib/dorothy/object.rb
@@ -5,5 +5,48 @@ class Z::Object
   def initialize( memory, number )
     @memory, @number = memory, number
   end
+
+  def parent?
+    !! parent
+  end
+
+  def children?
+    !! child
+  end
+
+  def siblings?
+    ! siblings.empty?
+  end
+
+  def children
+    return []  unless children?
+
+    [child] + child.siblings
+  end
+
+  def siblings
+    return []  unless parent?
+
+    first_child = parent.child
+
+    a, s = [first_child], first_child
+    a << s while s = s.sibling
+
+    a.reject! { |o| o.number == number }
+
+    a
+  end
+
+  def root
+    return self  unless parent?
+
+    p = parent
+    p = p.parent while p.parent
+    p
+  end
+
+  def inspect
+    "#<Z::Object number: #{number} name: #{name}>"
+  end
 end
 

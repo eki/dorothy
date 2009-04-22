ff6b578782e11c13b6b04078b3e1a386ebafa31f
Eric K Idema <eki@vying.org>
2009-04-22 10:25:45 -0400

    Added Memory#object, returns the name of an object when given its number.  This is temporary.

--- ext/dorothy/machine/machine.h
+++ ext/dorothy/machine/machine.h
@@ -32,6 +32,7 @@ VALUE memory_read_byte( VALUE self, VALUE addr );
 VALUE memory_read_word( VALUE self, VALUE addr );
 VALUE memory_read_string( VALUE self, VALUE addr );
 VALUE memory_read_string_array( VALUE self, VALUE addr, VALUE length );
+VALUE memory_get_object( VALUE self, VALUE n );
 void  memory_free( void * );
 
 VALUE machine_alloc( VALUE klass );
